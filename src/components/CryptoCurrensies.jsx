4d84eee3521af7f269cae29b4b381723481a1593
Sina Madadi <sinamadadi.work@gmail.com>
2023-02-12 20:01:06 +0330

    home page almost completed !

--- src/components/CryptoCurrensies.jsx
+++ src/components/CryptoCurrensies.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import { ConsoleSqlOutlined } from '@ant-design/icons'
+import { Card, Col, Input, Row } from 'antd'
+import ColumnGroup from 'antd/es/table/ColumnGroup'
+import Link from 'antd/es/typography/Link'
+import millify from 'millify'
+import React, { useEffect, useState } from 'react'
 
 const CryptoCurrensies = () => {
+
+    const url = 'https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '07ddc9666cmsh3183f582f7f1926p125c38jsn552afed858dc',
+            'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+        }
+    };
+    const [coins, setCoins] = useState()
+    useEffect(() => {
+        fetch(url, options)
+            .then(res => res.json())
+            .then(data => {
+                setCoins(data.data?.coins)
+                console.log(data.data?.coins)
+            })
+            .catch(err => console.error('error:' + err));
+    }, [])
+
     return (
-        <div>CryptoCurrensies</div>
+        <>
+            {/* <div className="search-crypto">
+                <Input
+                    placeholder="Search Cryptocurrency"
+                    onChange={(e) => setSearchTerm(e.target.value.toLowerCase())}
+                />
+            </div> */}
+            <Row gutter={[32, 32]} className="crypto-card-container">
+                {coins?.map((currency) => (
+                    <Col
+                        xs={24}
+                        sm={12}
+                        lg={6}
+                        className="crypto-card"
+                        key={currency.uuid}
+                    >
+
+                        {/* Note: Change currency.id to currency.uuid  */}
+                        <Link key={currency.uuid} to={`/crypto/${currency.uuid}`}>
+                            <Card
+                                title={`${currency.rank}. ${currency.name}`}
+                                extra={<img className="crypto-image" src={currency.iconUrl} />}
+                                hoverable
+                            >
+                                <p>Price: {millify(currency.price)}</p>
+                                <p>Market Cap: {millify(currency.marketCap)}</p>
+                                <p>Daily Change: {currency.change}%</p>
+                            </Card>
+                        </Link>
+                    </Col>
+                ))}
+            </Row>
+        </>
     )
 }
 
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,43 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
-import { useGetCryptosQuery } from '../services/cryptoApi';
-
-
-// const url = 'https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0';
-
-// const options = {
-//     method: 'GET',
-//     headers: {
-//         'X-RapidAPI-Key': '07ddc9666cmsh3183f582f7f1926p125c38jsn552afed858dc',
-//         'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
-//     }
-// };
-
-// fetch(url, options)
-//     .then(res => res.json())
-//     .then(json => console.log(json))
-//     .catch(err => console.error('error:' + err));
-
-
+import CryptoCurrensies from './CryptoCurrensies';
+import News from './News';
 const HomePage = () => {
-    const { data } = useGetCryptosQuery();
-    const globalStats = data?.data?.stats;
-    console.log(data);
 
-    // if (isFetching) return 'Loading ....';
+    const url = 'https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '07ddc9666cmsh3183f582f7f1926p125c38jsn552afed858dc',
+            'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
+        }
+    };
+    const [stats, setStats] = useState()
+    useEffect(() => {
+        fetch(url, options)
+            .then(res => res.json())
+            .then(data => {
+                setStats(data.data?.stats)
+                // console.log(data.data?.stats)
+            })
+            .catch(err => console.error('error:' + err));
+    }, [])
+
     return (
         <>
+            {/* {stats?.map((stats, i) => {
+                return <h1 key={i}>{stats}</h1>
+            })} */}
             <Typography.Title level={2} className='heading' >Global Crypto Stats</Typography.Title>
             <Row>
-                <Col span={12} > <Statistic title='Total Cryptocurrencies' value={millify(globalStats.total)} /> </Col>
-                <Col span={12} > <Statistic title='Total Exchanges' value={millify(globalStats.totalExchanges)} /> </Col>
-                <Col span={12} > <Statistic title='Total Market Cap' value={millify(globalStats.totalMarketCap)} /> </Col>
-                <Col span={12} > <Statistic title='Total 24h Volume' value={millify(globalStats.total24hVolume)} /> </Col>
-                <Col span={12} > <Statistic title='Total Markets' value={millify(globalStats.totalMarkets)} /> </Col>
-                {/* <Col span={12} > <Statistic title='Total Markets' value='5' /> </Col> */}
+                <Col span={12} > <Statistic title='Total Cryptocurrencies' value={millify(stats.total)} /> </Col>
+                <Col span={12} > <Statistic title='Total Coins' value={millify(stats.totalCoins)} /> </Col>
+                <Col span={12} > <Statistic title='Total Exchanges' value={millify(stats.totalExchanges)} /> </Col>
+                <Col span={12} > <Statistic title='Total Market Cap' value={millify(stats.totalMarketCap)} /> </Col>
+                <Col span={12} > <Statistic title='Total 24h Volume' value={millify(stats.total24hVolume)} /> </Col>
+                <Col span={12} > <Statistic title='Total Markets' value={millify(stats.totalMarkets)} /> </Col>
             </Row>
+            <div className='home-heading-container'>
+                <Typography.Title level={2} className='home-title' >Top 10 CryptoCurrensies in the world</Typography.Title>
+                <Typography.Title level={3} className='show-more'><Link to='/cryptocurrencies'>show more</Link></Typography.Title>
+            </div>
+            <CryptoCurrensies simplified />
+            <div className='home-heading-container'>
+                <Typography.Title level={2} className='home-title' >Latest Crypto News</Typography.Title>
+                <Typography.Title level={3} className='show-more'><Link to='/cryptocurrencies'>show more</Link></Typography.Title>
+            </div>
+            <News simplified />
         </>
     )
 }
